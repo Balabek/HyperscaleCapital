d0afcb5bcf20434a6473dad1974790e59903ba9d
Balabek <vonbabayan@gmail.com>
2024-12-02 13:02:06 +0300

    Init

---
+++ js/hyperscale.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+    // Fancy animation for hamburger menu
+    $('.da_nav_toggler').on('click', function(){
+        $('.animated-icon1').toggleClass('open');
+    });
+
+});
